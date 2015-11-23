c02f4d1e72998bcac5dabf1295d72b0649157d5b
Vincent Weevers <mail@vincentweevers.nl>
2015-11-23 12:03:05 +0100

    Clean dir before build, add test directory

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,15 @@
-var gulp = require('gulp')
-  , gulpif = require('gulp-if')
-  , babel = require('gulp-babel')
+const gulp = require('gulp')
+    , gulpif = require('gulp-if')
+    , babel = require('gulp-babel')
+    , rimraf = require('rimraf')
 
-gulp.task('build', () => {
-  let condition = (vinyl) => {
-    let file = vinyl.relative
+gulp.task('clean', (done) => {
+  rimraf(__dirname + '/{generators-test,generators}/**/*', done)
+})
+
+gulp.task('build:generator', () => {
+  const condition = (vinyl) => {
+    const file = vinyl.relative
     return file.slice(-3) === '.js' && file.indexOf('templates') < 0
   }
 
@@ -13,4 +18,15 @@ gulp.task('build', () => {
     .pipe(gulp.dest('generators'))
 })
 
-gulp.task('default', ['build'])
+gulp.task('build:test', () => {
+  const condition = (vinyl) => {
+    const file = vinyl.relative
+    return file.slice(-3) === '.js' && file.indexOf('fixtures') < 0
+  }
+
+  return gulp.src('test/**/*')
+    .pipe(gulpif(condition, babel()))
+    .pipe(gulp.dest('generators-test'))
+})
+
+gulp.task('build', ['build:generator', 'build:test'])
